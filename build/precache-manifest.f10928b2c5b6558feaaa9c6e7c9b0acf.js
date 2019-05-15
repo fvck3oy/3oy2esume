a7d597607e1c32d8dd6168eac058a6a477621017
Pumin Swangjang <fvck3oy@hotmail.com>
2019-05-15 12:32:05 +0700

    re fix

---
+++ build/precache-manifest.f10928b2c5b6558feaaa9c6e7c9b0acf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68d2170d8e374236eca0",
+    "url": "/static/js/main.4267ccfc.chunk.js"
+  },
+  {
+    "revision": "3e7582d27bbd68e354e2",
+    "url": "/static/js/2.3986223b.chunk.js"
+  },
+  {
+    "revision": "68d2170d8e374236eca0",
+    "url": "/static/css/main.95ff5121.chunk.css"
+  },
+  {
+    "revision": "3e7582d27bbd68e354e2",
+    "url": "/static/css/2.cbb20303.chunk.css"
+  },
+  {
+    "revision": "7d9159fd60476b585013b2b8a4c6ca9a",
+    "url": "/index.html"
+  }
+];
